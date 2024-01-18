b514b1275e4ef0ba43e3af930d1ae075ba7843aa
Najafsyed <156337418+Najafsyed@users.noreply.github.com>
2024-01-18 22:03:10 +0500

    Add files via upload

---
+++ najaf_midterm.py
@@ -0,0 +1,27 @@
+#syed muhammad najaf
+#midterm exam
+#Q1 - Write a Python program to do arithmetical operations addition and division
+a=int(input("enter number1: "))
+b=int(input('enter number2: '))
+sum=a+b
+print("the sum is: ",sum)
+
+c=int(input("numerater"))
+d=int(input("denominator"))
+answer =c/d
+print("the devision is : ,div")
+
+
+#Q2 - Write a Python program to find the area of a triangl
+#input 1
+a=int(input("enter the lenght of the base of traingle  : "))
+#input2
+b=int(input("the hight of th tirngle is:"))
+area=(1/2*b*a)
+print('the area of tringle is:' ,area)
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit
+#input1
+a=int(input("enter temprature in celsius"))
+#input2
+
